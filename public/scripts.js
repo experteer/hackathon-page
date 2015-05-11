bad6b7de58e05d5a8a1dd80f9a97bd95ccb0558c
Tomasz Grzelak <tomasz.grzelak@experteer.com>
2015-05-11 12:37:16 +0200

    Better page loading

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,7 @@
-jQuery(function($) { 
+$( window ).load(function() {
   $('.tell-me-more').click(function() {
     $('html, body').animate({scrollTop: $('section').first().offset().top}, 1000);
   })
   new WOW().init();
+  $('#loading_overlay').fadeOut().delay(1000).hide();
 });
